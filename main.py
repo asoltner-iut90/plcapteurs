985b1bc4f2b40e95cd9d313cfc418834d25b9c75
ElenaBERNARD <adrien.bernard@edu.univ-fcomte.fr>
2026-06-03 16:28:42 +0200

    Parser and stuff

---
+++ main.py
@@ -0,0 +1,9 @@
+import parse_data
+
+def main():
+    data = parse_data.parse_sensor_data('moyen_test_2.txt')
+    print(data)
+
+
+if __name__ == "__main__":
+    main()
---
+++ parse_data.py
@@ -0,0 +1,19 @@
+def parse_sensor_data(file_path: str) -> dict:
+    print(f"Parsing data from {file_path}...")
+    with open(file_path, 'r') as file:
+        lines = [line.strip() for line in file if line.strip()]
+    
+    num_sensors = int(lines[0])
+    num_zones = int(lines[1])
+    lifetimes = [float(x) for x in lines[2].split()]
+    
+    sensors = {}
+    for i in range(num_sensors):
+        sensors[i + 1] = [int(z) for z in lines[3 + i].split()]
+        
+    return {
+        "num_sensors": num_sensors,
+        "num_zones": num_zones,
+        "lifetimes": lifetimes,
+        "sensors": sensors
+    }
